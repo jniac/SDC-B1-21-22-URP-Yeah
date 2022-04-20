81f3b639303debc9d913c35ac551006e295580e1
UtopisteNjoly <nicolas.joly@supdecreation.com>
2022-04-20 11:53:29 +0200

    unj-TimeAttack

--- Assets/People/UtopisteNJoly/Scripts/unj_TimeAttackLevelManager.cs
+++ Assets/People/UtopisteNJoly/Scripts/unj_TimeAttackLevelManager.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class unj_TimeAttack_LevelManager : MonoBehaviour
+public class unj_TimeAttackLevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject gameOverScreen;
+    public float elapsedTime = 0f;
+    public float remainingTime = 3f;
+
     void Start()
     {
-        
+        gameOverScreen.SetActive(false);
+        elapsedTime = 0f;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        elapsedTime = elapsedTime + Time.deltaTime;
+        remainingTime = remainingTime - Time.deltaTime;
+
+        if (remainingTime < 0f)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        gameOverScreen.SetActive(true);
+        Time.timeScale = 0f;
     }
 }
