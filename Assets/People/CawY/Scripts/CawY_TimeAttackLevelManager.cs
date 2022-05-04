25a1d3ca1c2cbc7943fcc4646c02fcb2b81d7fc1
CawY <ozennenathan@gmail.com>
2022-05-04 09:00:59 +0200

    FixAsianDing

---
+++ Assets/People/CawY/Scripts/CawY_TimeAttackLevelManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CawY_TimeAttackLevelManager : MonoBehaviour
+{
+    public GameObject gameoverScreen;
+    public float elapsedTime = 0f;
+    public float remainingTime = 3f;
+
+    public TMPro.TextMeshProUGUI remainingText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameoverScreen.SetActive(false);
+        elapsedTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime = elapsedTime + Time.deltaTime;
+        remainingTime = Mathf.Clamp(remainingTime - Time.deltaTime, 0f, float.PositiveInfinity);
+
+        remainingText.text = $"time: {remainingTime:F1}s";
+
+        if (remainingTime <= 0f)
+        {
+            GameOver();
+        }
+    }
+    void GameOver()
+    {
+        gameoverScreen.SetActive(true);
+        Time.timeScale = 0f;
+    }
+}
